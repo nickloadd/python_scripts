46f88fce542f222eb45b422bc9aa84b8d57436f6
nickloadd <32330419+nickloadd@users.noreply.github.com>
2022-04-20 12:24:35 +0700

    Add files via upload
    Script to delete empty directories and older files

---
+++ ClearStash.py
@@ -0,0 +1,31 @@
+import time
+import os
+
+COUNT_DIRECTORIES = 0
+COUNT_FILES = 0
+
+days = 1 #Older than days
+old = time.time() - (days * 24 * 60 * 60) #Older in seconds
+
+#List of directories to script
+FOLDERS = [
+        "C:\Trash"
+          ]
+
+
+for DIR in FOLDERS:
+    for root, dirs, files in os.walk(DIR): #os.walk for root_directory, directory(cwd), files
+        if os.path.isdir(root):
+            if len(os.listdir(root)) == 0: #if directory is empty
+                print(root)
+                COUNT_DIRECTORIES += 1
+                #os.rmdir(root) #delete empty directories
+        for file in files:
+            path = root + "\\" + file
+            if old >= os.path.getctime(path): #if file older than days
+                print(path)
+                COUNT_FILES +=1
+                #os.remove(path) #delete old files
+
+#Statisctic
+print("Empty directories deleted: " + str(COUNT_DIRECTORIES) + "\n" + "Files older than " + str(days) + " days deleted: " + str(COUNT_FILES))
